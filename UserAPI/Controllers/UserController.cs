1a7cf1ccc65554aea5c894c0433f83dcdc2da400
Bruno de Jesus Souza <bruno.de.jesus.souza@avanade.com>
2025-09-29 10:30:43 -0300

    Add project files.

---
+++ UserAPI/Controllers/UserController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserAPI.Models;
+
+namespace UserAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // Demo in-memory user store
+        private static List<UserModel> users = new List<UserModel>();
+        private static int nextId = 1;
+
+        // GET: api/User
+        [HttpGet]
+        public ActionResult<IEnumerable<UserModel>> GetUsers()
+        {
+            return Ok(users);
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}")]
+        public ActionResult<UserModel> GetUser(int id)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+                return NotFound();
+            return Ok(user);
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public ActionResult<UserModel> CreateUser([FromBody] UserModel user)
+        {
+            user.Id = nextId++;
+            users.Add(user);
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+        }
+
+        // PUT: api/User/5
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(int id, [FromBody] UserModel updatedUser)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+                return NotFound();
+
+            user.Name = updatedUser.Name;
+            user.Email = updatedUser.Email;
+            return NoContent();
+        }
+
+        // DELETE: api/User/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(int id)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+                return NotFound();
+
+            users.Remove(user);
+            return NoContent();
+        }
+    }
+}
---
+++ UserAPI/Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace UserAPI.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+    }
+}
